a67ccc3e9f512eda2810283d303ba1c91668cc34
Shivam00123 <shivamjackson6@gmail.com>
2024-03-25 00:29:51 +0530

    made some fixes on the ui

--- portfolio/src/components/Details.tsx
+++ portfolio/src/components/Details.tsx
@@ -38,30 +38,35 @@ const Details = ({ params }: { params: any }) => {
     fetchData();
   }, []);
 
+  const openLink = (link: string) => {
+    if (!link) return;
+    window.open(link, "_blank");
+  };
+
   return (
     <Screen>
       {data ? (
-        <div className="w-full h-full min-h-screen px-10 lg:px-20 pb-20 relative">
-          <h1 className="font-semibold lg:leading-6 text-font-white text-4xl text-center sticky top-0 pt-7 h-20 bg-main-blue">
+        <div className="w-full h-full min-h-screen pb-20 relative">
+          <h1 className="w-screen font-semibold lg:leading-6 text-font-white text-4xl text-center sticky top-0 pt-7 h-20 bg-main-blue z-50">
             {data?.title} {data?.company ? `. ${data.company}` : ""}{" "}
             {data?.location}
           </h1>
-          <p className="text-sm text-paragraph tracking-wide my-10 text-justify whitespace-pre-line">
+          <p className="text-sm text-paragraph tracking-wide my-10 text-justify whitespace-pre-line px-10 lg:px-20">
             {`${data?.longDescription}`}
           </p>
           <h1
             style={{ margin: "50px 0px 10px 0px" }}
-            className="text-2xl text-font-white font-bold"
+            className="text-2xl text-font-white font-bold px-10 lg:px-20"
           >
             Key Contribution
           </h1>
-          <div className="w-full h-full flex flex-col lg:flex-row items-center gap-10 lg:gap-0">
+          <div className="w-full h-full flex flex-col lg:flex-row items-start gap-10 lg:gap-0 px-10 lg:px-20">
             <ul
               style={{
                 listStyleType: "disc",
                 marginLeft: "12px",
               }}
-              className="text-paragraph text-sm list-disc w-1/2 mr-10"
+              className="text-paragraph text-sm list-disc lg:w-1/2 mr-10"
             >
               {data?.points?.map((point: string, idx: any) => (
                 <li key={idx} className="mb-5">
@@ -69,30 +74,32 @@ const Details = ({ params }: { params: any }) => {
                 </li>
               ))}
             </ul>
-            <a
-              href={`${data?.link}`}
-              target="_blank"
-              onMouseEnter={() => setHoveringOnImage(true)}
-              onMouseLeave={() => setHoveringOnImage(false)}
-              className={`w-full lg:w-1/2 h-fit z-40 relative ${
-                hoveringOnImage ? "image-hover-on block" : "image-hover-off"
-              }`}
-            >
-              <Image
-                width={1000}
-                height={600}
-                src={`${data.image}`}
-                alt="dummy"
-                layout="responsive"
-              />
-              {hoveringOnImage && (
-                <div className="absolute bottom-0 left-0 text-font-white z-50 w-fit h-6 bg-main-blue text-[8px] font-bold p-2 rounded-tr-lg  text-center grid place-items-center">
-                  <h1 className="text-font-white">
-                    Click image to open website
-                  </h1>
-                </div>
-              )}
-            </a>
+
+            {data?.image && (
+              <div
+                onClick={() => openLink(`${data?.link}`)}
+                onMouseEnter={() => setHoveringOnImage(true)}
+                onMouseLeave={() => setHoveringOnImage(false)}
+                className={`w-full lg:w-1/2 h-fit z-40 relative ${
+                  hoveringOnImage ? "image-hover-on block" : "image-hover-off"
+                }`}
+              >
+                <Image
+                  width={1000}
+                  height={600}
+                  src={`${data.image}`}
+                  alt={`${data.image}`}
+                  layout="responsive"
+                />
+                {hoveringOnImage && data?.link && (
+                  <div className="absolute bottom-0 left-0 text-font-white z-50 w-fit h-6 bg-main-blue text-[8px] font-bold p-2 rounded-tr-lg  text-center grid place-items-center">
+                    <h1 className="text-font-white">
+                      Click image to open website
+                    </h1>
+                  </div>
+                )}
+              </div>
+            )}
           </div>
         </div>
       ) : (
--- portfolio/src/components/RightComponent.tsx
+++ portfolio/src/components/RightComponent.tsx
@@ -15,7 +15,7 @@ const RightComponent = () => {
       }}
       className="w-full h-full lg:pl-[40%] overflow-y-scroll py-20 text-paragraph flex justify-center lg:justify-end"
     >
-      <div className="w-full lg:w-3/5 h-full mx-10  lg:mr-[20%]">
+      <div className="w-full lg:w-3/5 h-full mx-10 my-20  lg:mr-[20%]">
         <About />
         <Experiences />
         <Resume />

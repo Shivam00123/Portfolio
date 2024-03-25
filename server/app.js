e959a3440c614b040a5a75b6e5e2380c27d64a35
Shivam00123 <shivamjackson6@gmail.com>
2024-03-25 17:10:23 +0530

    added cors to app file

--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const experienceRouter = require("./Routes/experienceRouter");
 const companyProjectsRouter = require("./Routes/companyProjectsRouter");
 const personalProjectsRouter = require("./Routes/personalProjectsRouter");
 const resumeRouter = require("./Routes/resumeRouter");
+
+app.use(cors());
+app.options("*", cors());
 
 app.use("/v1/experiences", experienceRouter);
 app.use("/v1/companyProjects", companyProjectsRouter);
